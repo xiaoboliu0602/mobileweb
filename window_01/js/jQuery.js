4d1ec94a4526c3fb40750fd7147403ca58f2233b
xiaobo <xiaoboliu0602@163.com>
2020-04-27 18:54:51 +0800

    A simple web

--- window_01/js/jQuery.js
+++ window_01/js/jQuery.js
@@ -1,10 +1,11 @@
 /* jQuery.js */
 
-document.write("测试 JavaScript 外部脚本！<br>");
+// document.write("测试 JavaScript 外部脚本！<br>");
 
 $(document).ready(function(){
 
     // 编写jQuery方法...
  
  });
- 
+
+
